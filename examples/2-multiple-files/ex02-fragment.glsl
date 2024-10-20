17c82ddd7f1dca10ccc8019418fcbf5428a13acd
Dan M <danmikhail@gmail.com>
2024-10-20 16:28:58 -0400

    examples up through 3.4

---
+++ examples/2-multiple-files/ex02-fragment.glsl
@@ -0,0 +1,6 @@
+#version 300 es
+precision highp float;
+out vec4 anyNameYouWant;
+void main() {
+    anyNameYouWant = vec4(1, 0, 0.5, 1);
+}
---
+++ examples/2-multiple-files/ex02-vertex.glsl
@@ -0,0 +1,7 @@
+#version 300 es
+void main(){
+  gl_Position=vec4(sin(float(gl_VertexID)),
+  cos(float(gl_VertexID)),
+  0,
+1);
+}
