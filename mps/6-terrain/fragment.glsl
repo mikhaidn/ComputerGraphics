26fe9f93fa66d22ba30130d66b143470fd43de12
Dan M <danmikhail@gmail.com>
2024-11-24 23:38:09 -0500

    update progress [todo-Verify normals, Render and light]

--- mps/6-terrain/fragment.glsl
+++ mps/6-terrain/fragment.glsl
@@ -3,6 +3,21 @@ precision highp float;
 uniform vec4 color;
 out vec4 fragColor;
 in vec4 color2;
-void main() {
-    fragColor = color2;
+in vec4 normals2;
+void main(){
+    vec3 lightDir=normalize(vec3(1.,1.,1.));
+    
+    // Get normal from interpolated vertex normal
+    vec3 normal=normalize(normals2.xyz);
+    
+    // Calculate diffuse lighting
+    float diffuse=max(dot(normal,lightDir),0.);
+    
+    // Add ambient light to avoid completely dark areas
+    float ambient=.2;
+    
+    // Final color combines ambient and diffuse lighting
+    vec3 finalColor=color2.rgb*(ambient+diffuse);
+    
+    fragColor=vec4(finalColor,1.);
 }
--- mps/6-terrain/vertex.glsl
+++ mps/6-terrain/vertex.glsl
@@ -1,11 +1,14 @@
 #version 300 es
 layout(location=0)in vec4 position;
 layout(location=1)in vec4 color;
+layout(location=2)in vec4 normals;
 out vec4 color2;
+out vec4 normals2;
 uniform mat4 m;
 uniform mat4 v;
 uniform mat4 p;
 void main(){
     gl_Position=p*v*m*position;
     color2=color;
+    normals2=normals;
 }
